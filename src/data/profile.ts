dd4161ee840d611d96bff9e9c31ac17b43435485
Amirreza Hajizadeh <amirreza.haji.0926264710@gmail.com>
2026-09-18 15:05:21 -0400

    feat: link academic CV download on home and about pages

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -31,6 +31,7 @@ export const profile: Profile = {
   timeZone: 'Asia/Tehran',
   emails: ['amirrezahajizadeh2002@gmail.com', 'amirreza.haji.0926264710@gmail.com'],
   phones: ['+98-930 634 1814', '+98-915 228 8744'],
+  academicResume: '/Academic_CV.pdf',
   // Static imports produce a new asset URL whenever the image contents change.
   photo: profilePicture.src,
 };
